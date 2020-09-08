70fe56168e3de85d1a74aa04e871febb26b8a6b0
mhpMHP27167 <57695577+mhpMHP27167@users.noreply.github.com>
2020-09-08 16:13:20 -0400

    added images and improved css

--- js/index.js
+++ js/index.js
@@ -5,3 +5,13 @@ $(".container").hover(function(){
       background, black
     });
   });
+
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
